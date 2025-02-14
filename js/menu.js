161296b41ad2e10bff1df86a0aee7a930429db42
Jc-Arroyo <jcarroyos@gmail.com>
2025-02-14 19:01:10 +0000

    Eliminar enlace a la página de Paula duplicado del menú de navegación

--- js/menu.js
+++ js/menu.js
@@ -16,7 +16,6 @@ document.addEventListener('DOMContentLoaded', function() {
                     <li><a href="${baseUrl}/pages/samuel.html">samuel</a></li>
                     <li><a href="${baseUrl}/pages/sillydata.html">sillydata</a></li>
                     <li><a href="${baseUrl}/pages/jpverahm1.html">jpverahm1</a></li>
-                    <li><a href="${baseUrl}/pages/paula.html">paula</a></li>
                     <li><a href="${baseUrl}/pages/vpinedapu.html">vpinedapu</a></li>
                     <!-- Agrega más enlaces según sea necesario -->
                 </ul>
